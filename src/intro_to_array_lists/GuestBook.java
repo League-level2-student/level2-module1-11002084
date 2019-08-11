9e030004921abc5e286b049bc8bfb170b45a2439
League Student <league@iMac-23.attlocal.net>
2019-08-11 15:58:48 -0700

    Guest Book

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,62 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton buttonAddNames = new JButton();
+	JButton buttonViewNames = new JButton();
+	ArrayList<String> names = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		new GuestBook().createUI();
+	}
+
+	void createUI() {
+		buttonAddNames.setText("Add Name");
+		buttonViewNames.setText("View Names");
+		buttonAddNames.addActionListener(this);
+		buttonViewNames.addActionListener(this);
+		panel.add(buttonAddNames);
+		panel.add(buttonViewNames);
+		frame.setName("Hitlist");
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == buttonAddNames) {
+			ArrayList<String> names = new ArrayList<String>();
+			String nameAdded = JOptionPane.showInputDialog("Input name to add to list.");
+			names.add(nameAdded);
+		} else {
+			for (int i = 0; i < names.size(); i++) {
+				System.out.println("Guest #" + i + " : " + names.get(i));
+			}
+		}
+
+	}
 }
